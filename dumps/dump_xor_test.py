29e52ea4a76ea54d19c0289208d6e1ebb721feb5
Aluriak <lucas.bourneuf@laposte.net>
2017-01-21 22:31:24 +0100

    dumps: move global level code to main module

--- dumps/dump_xor_test.py
+++ dumps/dump_xor_test.py
@@ -14,29 +14,30 @@ def convert_to_bool(bit):
     return bit == '1'
 
 
-with open('data/mini_dump.txt', 'rb') as infd:
+if __name__ == '__main__':
+    with open('data/mini_dump.txt', 'rb') as infd:
 
-    key = infd.readline()
-    key_b = BitArray(key).bin
-    assert infd.readline().decode('utf8').strip() == '24hc17'
-    line = infd.readline()
-    line_b = BitArray(line).bin
-    xor_line = ''
-    # print(BitArray(key).bin)
-    # print(BitArray(line).bin)
-    for group in grouper(line_b, len(key_b)):
-        if None in group:
-            print("Padding : ", group)
-            break
-        for bkey, bline in zip(key_b, group):
-            if convert_to_bool(bkey) != convert_to_bool(bline):
-                xor_line += '1'
-            else:
-                xor_line += '0'
+        key = infd.readline()
+        key_b = BitArray(key).bin
+        assert infd.readline().decode('utf8').strip() == '24hc17'
+        line = infd.readline()
+        line_b = BitArray(line).bin
+        xor_line = ''
+        # print(BitArray(key).bin)
+        # print(BitArray(line).bin)
+        for group in grouper(line_b, len(key_b)):
+            if None in group:
+                print("Padding : ", group)
+                break
+            for bkey, bline in zip(key_b, group):
+                if convert_to_bool(bkey) != convert_to_bool(bline):
+                    xor_line += '1'
+                else:
+                    xor_line += '0'
 
-    char_line = ''
-    for group in grouper(xor_line, 8):
-        t = int("".join(group))
-        char_line += chr(t)
+        char_line = ''
+        for group in grouper(xor_line, 8):
+            t = int("".join(group))
+            char_line += chr(t)
 
-    print(char_line)
+        print(char_line)
