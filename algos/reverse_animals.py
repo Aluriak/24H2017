1ee504cd55bb75747a947c26d8a45bea9a6f4fa5
Aluriak <lucas.bourneuf@laposte.net>
2017-01-21 19:01:20 +0100

    algos: fix last line problem

--- algos/reverse_animals.py
+++ algos/reverse_animals.py
@@ -3,7 +3,7 @@
 
 
 def run(payload):
-    animals = tuple(payload.splitlines())
+    animals = tuple(payload.split().splitlines())
 
     rev_animals = tuple(''.join(reversed(name)) for name in animals)
     sorted_rev = sorted(rev_animals)
