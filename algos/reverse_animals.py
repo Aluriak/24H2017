9b9d005103b38e7ad50d06dd406235e0db04d882
Aluriak <lucas.bourneuf@laposte.net>
2017-01-21 18:52:13 +0100

    algos: reverse animals imple

---
+++ algos/reverse_animals.py
@@ -0,0 +1,13 @@
+
+
+
+
+def run(payload):
+    animals = tuple(payload.splitlines())
+
+    rev_animals = tuple(reversed(name) for name in animals)
+    sorted_rev = sorted(rev_animals)
+    return '\n'.join(''.join(reversed(name))
+                     for name in sorted_rev)
+
+
