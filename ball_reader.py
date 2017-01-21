1a1e1e726d190889889078c212bd02bd6c09a1d7
RomainFeron <romain.feron@evobio.eu>
2017-01-21 11:36:35 +0100

    ball reader

---
+++ ball_reader.py
@@ -0,0 +1,28 @@
+import serial
+
+DEFAULT_OUTPUT_FILE = 'dump.txt'
+DEFAULT_PORT_NAME = '/dev/ttyUSB0'
+
+
+def gen_bytes(port_name=DEFAULT_PORT_NAME):
+
+    connection = serial.Serial(port_name)  # open serial port
+    print(connection.name)         # check which port was really used
+
+    while True:
+
+        yield connection.read()     # write a string
+
+    connection.close()             # close port
+
+
+def write_bytes(source, output_file=DEFAULT_OUTPUT_FILE):
+
+    source = iter(source)
+    with open(output_file, 'wb') as output:
+        output.write(next(gen_bytes()))
+
+
+if __name__ == '__main__':
+
+    write_bytes(gen_bytes(DEFAULT_PORT_NAME), DEFAULT_OUTPUT_FILE)
